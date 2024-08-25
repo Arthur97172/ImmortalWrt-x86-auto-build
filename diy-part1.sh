7b94129e2348af8876f955f7f2cee6b75b00f998
Arthur97172 <arthur97172@outlook.com>
2024-08-25 14:43:31 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -17,7 +17,6 @@
 
 echo 'src-git wukibaka_packages https://github.com/wukibaka/openwrt-packages' >>feeds.conf.default
 echo 'src-git kiddin9 https://github.com/kiddin9/openwrt-packages'  >>feeds.conf.default
-echo 'src-git wukibaka_packages https://github.com/wukibaka/openwrt-packages'  >>feeds.conf.default
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages'  >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small'  >>feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
