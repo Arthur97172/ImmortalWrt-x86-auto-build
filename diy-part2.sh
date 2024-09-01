14b2a90460bb78262d860695454698fffd2ed0f8
Arthur97172 <arthur97172@outlook.com>
2024-09-01 15:03:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,12 +10,6 @@
 # See /LICENSE for more information.
 #
 
-
-#加入turboacc
-curl -sSL https://raw.githubusercontent.com/chenmozhijin/turboacc/luci/add_turboacc.sh -o add_turboacc.sh
-chmod -R 777 add_turboacc.sh
-./add_turboacc.sh
-
 # 移除要替换的包
 rm -rf feeds/luci/applications/luci-app-openclash
 git clone -b dev --depth 1 https://github.com/vernesong/OpenClash.git /tmp/OpenClash
